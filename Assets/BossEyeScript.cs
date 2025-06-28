acefa9e04d42cb5c966be001bcb517fe217447c5
xSocram <marcos8frias@gmail.com>
2025-06-28 15:42:12 -0300

    Animaciones jefe

--- Assets/BossEyeScript.cs
+++ Assets/BossEyeScript.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 public class BossEyeScript : MonoBehaviour
 {
     [SerializeField] private BossController bossController;
+    private Animator animator;
+    [SerializeField] private bool isNose;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("hit") || collision.CompareTag("iceMagic"))
+        int rocklayer = LayerMask.NameToLayer("RockMagic");
+
+        if (collision.CompareTag("hit") || collision.CompareTag("iceMagic") || collision.gameObject.layer==rocklayer)
         {
             bossController.RegisterHit();
+            if (isNose) 
+            {
+                animator.SetTrigger("Hit");
+            }
         }
+    }
+    public void PlayHitAnimation()
+    {
+        animator.SetTrigger("Hit");
     }
 
 }
